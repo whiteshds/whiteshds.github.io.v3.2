6db16b76fe0ac13a2bff22e82515fc72c4ad1b7c
whiteshds <2689658133@qq.com>
2020-10-21 09:06:14 +0800

    Site updated: 2020-10-21 09:06:13

--- js/gallerysuijitupian.js
+++
@@ -1,8 +0,0 @@
-/**
- * Minified by jsDelivr using Terser v3.14.1.
- * Original file: /gh/volfclub/travellings@1.2.3/assets/travelling.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-function gallerysuijitupian(){var t=new Array;if(t[0]="https://whiteshds.ml/Gallery/01/",t[1]="https://whiteshds.ml/Gallery/02/",t[2]="https://whiteshds.ml/Gallery/03/",t[3]="https://whiteshds.ml/Gallery/04/",document.referrer){var s=new URL(document.referrer).origin;t.splice(t.indexOf(s),1)}var h=Math.floor(Math.random()*t.length);window.location=t[h]}
-//# sourceMappingURL=/sm/05e37858c0ab43b7e3ab355f84416a73d455686609ecb4bf66ec44d74b4a63dc.map
